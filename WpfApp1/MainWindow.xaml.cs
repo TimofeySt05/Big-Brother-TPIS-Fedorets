1d4d7bde12434f284fa7426fb4f8ecd9993f5d3a
ul1727 <140564297+ul1727@users.noreply.github.com>
2024-03-03 21:18:33 +1000

    Попытка вставить видео, ничего не работает

--- WpfApp1/MainWindow.xaml.cs
+++ WpfApp1/MainWindow.xaml.cs
@@ -16,6 +16,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Windows.Forms;
 
 namespace WpfApp1
 {
@@ -31,12 +32,60 @@ namespace WpfApp1
         {
             PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(prop));
         }
+        private MediaElement video_source;
+
+        public MediaElement Video_source
+        {
+            get { return video_source; }
+            set { 
+                video_source = value;
+                OnPropertyChanged("Video_source");
+            }
+        }
+
+        private string video_name;
+        public string Video_name
+        {
+            get { return video_name; } 
+            set 
+            { 
+                video_name = value;
+                MediaElement X = new MediaElement
+                {
+                    Source = new Uri(value, UriKind.Relative)
+                };
+                Video_source= X;
+                OnPropertyChanged("Video_name"); 
+            }
+        }
+
     }
     public partial class MainWindow : Window
     {
+        string Path;
         public MainWindow()
+
         {
+            this.DataContext = new Video();
             InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            //FolderBrowserDialog fbd = new FolderBrowserDialog();
+            //fbd.ShowDialog();
+            //Path = fbd.SelectedPath;
+            //if (Path != null)
+            //{
+            //    (DataContext as Video).Video_name = Path;
+            //}
+
+            OpenFileDialog ofd = new OpenFileDialog();
+            ofd.ShowDialog();
+            Path = ofd.FileName;
+
+
+
         }
     }
 }
