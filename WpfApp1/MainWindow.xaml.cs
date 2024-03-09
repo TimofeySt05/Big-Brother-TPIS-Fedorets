65afadb7fba03be3f6771669e474e66ff45ba2a9
ul1727 <140564297+ul1727@users.noreply.github.com>
2024-03-09 14:01:43 +1000

    Сделала слайд-шоу из картинок в выбранной папке

--- WpfApp1/MainWindow.xaml.cs
+++ WpfApp1/MainWindow.xaml.cs
@@ -1,9 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel;
+using System.Diagnostics;
 using System.IO;
 using System.Linq;
-using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -11,12 +10,25 @@ using System.Windows.Controls;
 using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
-using System.Windows.Interop;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Microsoft.Win32;
+using System.Drawing;
+using static System.Net.Mime.MediaTypeNames;
 using System.Windows.Forms;
+using System.Xml.Linq;
+using System.Windows.Interop;
+using System.Security.Cryptography;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Runtime.Serialization;
+using static System.Net.WebRequestMethods;
+using System.Windows.Threading;
+
+
+
 
 namespace WpfApp1
 {
@@ -32,12 +44,13 @@ namespace WpfApp1
         {
             PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(prop));
         }
-        private MediaElement video_source;
+        private BitmapSource video_source;
 
-        public MediaElement Video_source
+        public BitmapSource Video_source
         {
             get { return video_source; }
-            set { 
+            set
+            {
                 video_source = value;
                 OnPropertyChanged("Video_source");
             }
@@ -46,46 +59,77 @@ namespace WpfApp1
         private string video_name;
         public string Video_name
         {
-            get { return video_name; } 
-            set 
-            { 
+            get { return video_name; }
+            set
+            {
                 video_name = value;
-                MediaElement X = new MediaElement
-                {
-                    Source = new Uri(value, UriKind.Relative)
-                };
-                Video_source= X;
-                OnPropertyChanged("Video_name"); 
+                string[] photonames = new string[] { ".jpg", ".png", ".jpeg", ".tiff", ".gif", ".bmp", ".ico", ".webp", ".raw" };
+                if (!string.IsNullOrEmpty(video_name) && System.IO.File.Exists(video_name) && Directory.Exists(System.IO.Path.GetDirectoryName(video_name)))
+                { //1
+                    if (photonames.Contains(System.IO.Path.GetExtension(value)))
+                    {
+
+                        BitmapImage X = new BitmapImage();
+                        X.BeginInit();
+                        X.UriSource = new Uri(value, UriKind.RelativeOrAbsolute);
+                        X.EndInit();
+                        Video_source = X;
+
+                    }
+                    else
+                    {
+                        var icon = System.Drawing.Icon.ExtractAssociatedIcon(value);
+                        BitmapSource X1 = Imaging.CreateBitmapSourceFromHIcon(
+                            icon.Handle, System.Windows.Int32Rect.Empty,
+                            BitmapSizeOptions.FromEmptyOptions());
+                        Video_source = X1;
+                    }
+                }
             }
         }
-
     }
-    public partial class MainWindow : Window
-    {
-        string Path;
-        public MainWindow()
 
+                
+
+        public partial class MainWindow : Window
         {
-            this.DataContext = new Video();
+
+        string Path;
+        int currentIndex = 0;
+        string[] fileNames;
+        private DispatcherTimer timer;
+
+        public MainWindow()
+        {
             InitializeComponent();
+            this.DataContext = new Video();
+            timer = new DispatcherTimer();
+            timer.Interval = TimeSpan.FromSeconds(2);
+            timer.Tick += Timer_Tick;
+            timer.Start();
         }
 
         private void Button_Click(object sender, RoutedEventArgs e)
         {
-            //FolderBrowserDialog fbd = new FolderBrowserDialog();
-            //fbd.ShowDialog();
-            //Path = fbd.SelectedPath;
-            //if (Path != null)
-            //{
-            //    (DataContext as Video).Video_name = Path;
-            //}
-
-            OpenFileDialog ofd = new OpenFileDialog();
-            ofd.ShowDialog();
-            Path = ofd.FileName;
-
-
-
+            FolderBrowserDialog fbd = new FolderBrowserDialog();
+            fbd.ShowDialog();
+            Path = fbd.SelectedPath;
+            if(Path!="" && Path!=null) fileNames = Directory.GetFiles(Path).ToArray();
+        }
+        private void Timer_Tick(object sender, EventArgs e)
+        {
+            if (Path != "" && Path!=null)
+            {
+                if (currentIndex < fileNames.Length)
+                {
+                    (DataContext as Video).Video_name = fileNames[currentIndex];
+                    currentIndex++;
+                }
+                else
+                {
+                    currentIndex=0;
+                }
+            }
         }
     }
-}
+ }
