4cee765b4fe5afc25b886c9bc3a42c17df66cf28
TimofeySt05 <161436819+TimofeySt05@users.noreply.github.com>
2024-03-14 11:54:52 +1000

    Попытка исправления 1

--- WpfApp1/MainWindow.xaml.cs
+++ WpfApp1/MainWindow.xaml.cs
@@ -91,52 +91,88 @@ namespace WpfApp1
         }
     }
 
-    
 
-        public partial class MainWindow : System.Windows.Window
+
+    public partial class MainWindow : System.Windows.Window
     {
 
-        string Path;
+        string Path = "";
         int currentIndex = 0;
         string[] fileNames;
+        double Sec = 0.05;
+        List<BitmapSource> Frames = new List<BitmapSource>();
         private DispatcherTimer timer;
-        
+        int flag = 0;
+
 
 
         public MainWindow()
         {
             InitializeComponent();
             this.DataContext = new Video();
+
             timer = new DispatcherTimer();
-            timer.Interval = TimeSpan.FromSeconds(2);
+            timer.Interval = TimeSpan.FromSeconds(Sec);
             timer.Tick += Timer_Tick;
             timer.Start();
-            
-            
+
         }
 
-        
+        public static BitmapSource Convert(System.Drawing.Bitmap bitmap)
+        {
+            var bitmapData = bitmap.LockBits(
+                new System.Drawing.Rectangle(0, 0, bitmap.Width, bitmap.Height),
+                System.Drawing.Imaging.ImageLockMode.ReadOnly, bitmap.PixelFormat);
+            var bitmapSource = BitmapSource.Create(
+                bitmapData.Width, bitmapData.Height,
+                bitmap.HorizontalResolution, bitmap.VerticalResolution,
+                PixelFormats.Bgr24, null,
+                bitmapData.Scan0, bitmapData.Stride * bitmapData.Height, bitmapData.Stride);
+            bitmap.UnlockBits(bitmapData);
+            return bitmapSource;
+        }
 
         private void Button_Click(object sender, RoutedEventArgs e)
         {
+            flag = 1;
             FolderBrowserDialog fbd = new FolderBrowserDialog();
             fbd.ShowDialog();
             Path = fbd.SelectedPath;
-            if(Path!="" && Path!=null) fileNames = Directory.GetFiles(Path).ToArray();
+            if (Path != "" && Path != null) fileNames = Directory.GetFiles(Path).ToArray();
         }
         private void Timer_Tick(object sender, EventArgs e)
         {
-            if (Path != "" && Path!=null)
+            if (flag == 1)
             {
-                if (currentIndex < fileNames.Length)
+                if (Path != "" && Path != null)
                 {
-                    (DataContext as Video).Video_name = fileNames[currentIndex];
-                    currentIndex++;
+                    if (currentIndex < fileNames.Length)
+                    {
+                        (DataContext as Video).Video_name = fileNames[currentIndex];
+                        currentIndex++;
+                    }
+                    else
+                    {
+                        currentIndex = 0;
+                    }
                 }
-                else
+            }
+            else if (flag == 2)
+            {
+                if (Path != "" && Path != null)
                 {
-                    currentIndex=0;
+                    if (currentIndex < Frames.Count)
+                    {
+                        (DataContext as Video).Video_source = Frames[currentIndex];
+                        Console.WriteLine($"{currentIndex}  " + Frames[currentIndex]);
+                        currentIndex++;
+                    }
+                    else
+                    {
+                        currentIndex = 0;
+                    }
                 }
+
             }
         }
 
@@ -144,34 +180,31 @@ namespace WpfApp1
         {
             System.Windows.Forms.OpenFileDialog fbd = new System.Windows.Forms.OpenFileDialog(); //выбор файла. Есть в папке с проектом
             fbd.ShowDialog();
-            string VPath = fbd.FileName;
-
-            var videoFile = VPath;
+            Path = fbd.FileName;
+            var videoFile = Path;
             var capture = new VideoCapture(videoFile);
-            var window = new OpenCvSharp.Window("Video Frame by Frame");  //для вывода в окно
+            //var window = new OpenCvSharp.Window("Video Frame by Frame");  //для вывода в окно
             var image = new Mat();
-            var dic_image = new Dictionary<int, Bitmap>(); // словарь с кадрами
-
+            //var dic_image = new Dictionary<int, Bitmap>(); // словарь с кадрами
             int i = 0;
-            int i_nom = 0;
-            
+
+
             while (capture.IsOpened()) //Получение кадров
             {
+
                 capture.Read(image);
                 if (image.Empty()) break;
-                i++;
-                if (i % 30 == 0) // берем 1 из 30 кадров
+                if (i % 3 == 0)
                 {
-                    Bitmap frame = OpenCvSharp.Extensions.BitmapConverter.ToBitmap(image); // в битмап
-                    dic_image[i_nom] = frame;  // словарь с кадрами
-                    i_nom++;
-                    window.ShowImage(image);  //для вывода в окно
-                    if (Cv2.WaitKey(1) == 113) // Q
-                        break;
-                }
+                    BitmapSource frame = Convert(OpenCvSharp.Extensions.BitmapConverter.ToBitmap(image)); // в битмап
+                    Frames.Add(frame);
+                }//добавляем конвертированный в битмап сурс битмап в список битмапов
+                i++;
+                //window.ShowImage(image);  //для вывода в окно
             }
-            window.Close();
-            Console.WriteLine(dic_image.Count);
+            flag = 2;
+            Console.WriteLine(Frames.Count);
         }
+        //test
     }
- }
+}
